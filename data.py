a44cca1bb5afad3acbf4191d2b8105aa5f90c427
amazingDD <1172392977@gmail.com>
2025-01-14 17:40:27 +0800

    add imagenet interface

--- data.py
+++ data.py
@@ -1,5 +1,39 @@
+import os
+import pickle
+from PIL import Image
+
+import torch
 import torchvision
 import torchvision.transforms as transforms
+from torch.utils.data import Dataset
+
+class TinyImageNetDataset(Dataset):
+    def __init__(self, root='./tiny-imagenet-200', train=True, transform=None):
+        super().__init__()
+
+        self.root = root
+        self.transform = transform
+        self.train = train
+
+        if self.train:
+            with open(os.path.join(self.root, 'tiny-imagenet-200' , 'train_dataset.pkl'), 'rb') as f:
+                self.data, self.targets = pickle.load(f)
+        else:
+            with open(os.path.join(self.root, 'tiny-imagenet-200', 'val_dataset.pkl'), 'rb') as f:
+                self.data, self.targets = pickle.load(f)
+
+        self.targets = self.targets.type(torch.LongTensor)
+
+    def __getitem__(self, index):
+        data = self.data[index].permute(1, 2, 0).numpy()
+        data = Image.fromarray(data)
+        if self.transform:
+            data = self.transform(data)
+
+        return data, self.targets[index] 
+    
+    def __len__(self):
+        return len(self.targets)
 
 def load_data(name='cifar10'):
     if name == 'cifar10':
@@ -30,8 +64,27 @@ def load_data(name='cifar10'):
         )
 
         C, H, W, num_classes = 3, 32, 32, 10
+
+    if name == 'imagenet':
+        transform_train = transforms.Compose([
+            transforms.Resize(64, interpolation=transforms.InterpolationMode.BICUBIC),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+        ])
+
+        transform_test = transforms.Compose([
+            transforms.Resize(64, interpolation=transforms.InterpolationMode.BICUBIC),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+        ])
+
+        train_set = TinyImageNetDataset('./data', train=True, transform=transform_train)
+        test_set = TinyImageNetDataset('./data', train=False, transform=transform_test)
+
+        C, H, W, num_classes = 3, 64, 64, 200
     else:
         raise ValueError(f'Invalid dataset name: {name}')
+    
     
     return train_set, test_set, C, H, W, num_classes
 
